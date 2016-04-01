046948b5bfa41390b4d4031f3581e545f9fed7f6
BenByer <benbyer@shaw.ca>
2016-04-01 16:12:36 -0700

    Candidates updated with REPL

--- filters.rb
+++ filters.rb
@@ -1,11 +1,73 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
-
-end
-
-def experienced?(candidate)
-end
-
 # More methods will go below
+
+def getIdFromString(option)
+  str = option.split(" ")
+  str[1].to_i
+end
+
+def find(id)  
+  #binding.pry
+  @candidates.each { |candidate| 
+    if candidate[:id] == id 
+      return candidate
+    end
+
+  }
+  return "candidate #{id} not found"
+end
+
+def experienced?(candidate, experience=2)
+  candidate[:years_of_experience] >= experience
+end
+
+def valid_github_points?(candidate, points)
+  candidate[:github_points] >= points
+end
+
+def has_languages?(candidate, language_array)
+  lang_count = 0
+  language_array.each { |lang| 
+    if candidate[:languages].include?(lang)
+      return true
+    end    
+  }
+  return false
+
+end
+
+def recently_applied?(candidate, days)
+  candidate[:date_applied] > days.days.ago.to_date
+end
+
+def isNoob?(candidate, age)
+  candidate[:age] > age
+end
+
+def isValidCandidate?(candidate, years, git_points, languages, days_applied, age)
+  experienced?(candidate, years) && 
+  valid_github_points?(candidate, git_points) && 
+  has_languages?(candidate, languages) &&
+  recently_applied?(candidate, days_applied) &&
+  isNoob?(candidate, age)
+end
+
+def qualified_candidates
+  validCandidates = []
+  @candidates.select { |candy| 
+    if isValidCandidate?(candy, 2, 100, ['Ruby', 'Python'], 15, 17)
+      validCandidates << candy
+    end
+  }
+  #byebug
+  s = validCandidates.join(" ")
+end
+
+def ordered_by_qualifications(candies)
+  orderedCandidates = candies.sort_by { |hash| [ -hash[:years_of_experience], -hash[:github_points] ] }
+  #byebug
+  s = orderedCandidates.join(" ")
+end
+
--- main.rb
+++ main.rb
@@ -2,11 +2,49 @@
 # It requires the other files/gems that it needs
 
 require 'pry'
-require './candidates'
-require './filters'
+require 'byebug'
+require_relative './candidates'
+require_relative './filters'
 
 ## Your test code can go here
 
-# binding.pry
 
-pp qualified_candidates
+#binding.pry
+# pp experienced?(@candidates[2])
+# puts
+# pp find(10)
+# puts
+# pp qualified_candidates
+# puts
+# pp ordered_by_qualifications(@candidates)
+
+def menu
+  puts "Please make a selection"
+  puts
+  puts "(find)      Find first candidate"
+  puts "(all)       Show all candidates"
+  puts "(qualified) Show only qualified candidates"
+  puts "(quit)      Quit program"
+  
+end
+
+def process(option)  
+  if option.include? "find"
+    pp find(getIdFromString(option))
+  elsif option == "all"
+    pp ordered_by_qualifications(@candidates)
+  elsif option == "qualified"
+    pp qualified_candidates  
+  elsif option == "quit"
+    reset
+  else
+    puts "I don't know what that is"
+  end
+end
+
+loop do
+  menu
+  option = gets.chomp
+  break if option == "quit"
+  process(option)
+end
